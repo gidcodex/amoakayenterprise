3c2e6e94b3a7ec5b7428983ada3c0578b2f6d30a
Gideon Antwi <antwigideon01@gmail.com>
2026-06-11 14:45:01 +0200

    Initial commit

---
+++ inngest/functions.js
@@ -0,0 +1,57 @@
+import {inngest} from './client'
+import primsa from '@/lib/prisma' 
+
+// Inggest function to save user data to a database
+export const syncUserCreation = inngest.createFunction( 
+    {id: 'sync-user-create'},
+    {event: 'clerk/user.created'},
+    async ({ event }) => {
+        const {data} = event
+        await primsa.user.create ({
+            data: {
+                id: data.id,
+                email: data.email.addresses[0].email_address,
+                name:`${data.first_name} ${data.last_name}`,
+                image: data.image_url,
+
+            }
+        })
+
+    }
+
+)
+
+// Inngest Function to update user data in database
+export const syncUserUpdation = InngestFunctionReference.createFunction(
+    {id: 'sync-user-update'},
+    {event: 'clerk/user.updated'},
+    async ({ event }) => {
+        const {data} = event
+        await prisma.user.update({
+            where: {id: data.id,},
+              data: {
+                email: data.email.addresses[0].email_address,
+                name:`${data.first_name} ${data.last_name}`,
+                image: data.image_url,
+            }
+                
+            
+        })
+
+    }
+
+)
+
+// Inngest Function to delete user from database
+export const syncUserDeletion = inngest.createFunction(
+    {id: 'sync-user-delete'},
+    {event: 'clerk/user.deleted'},
+    async ({ event }) => {
+        const {data} = event
+        await prisma.user.delete({
+                where: {id: data.id,}
+            
+        })
+
+    }
+)
